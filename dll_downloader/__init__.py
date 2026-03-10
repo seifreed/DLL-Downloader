0f64a74e6b073d41fab3ce32c86b5ed35e4f3ad0
Marc R. <mriverolopez@gmail.com>
2026-03-10 17:14:31 +0100

    Bump version to 1.0.1

--- dll_downloader/__init__.py
+++ dll_downloader/__init__.py
@@ -5,6 +5,6 @@ This package provides a structured approach to downloading DLL files
 using Clean Architecture principles with clear separation of concerns.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ["__version__"]
